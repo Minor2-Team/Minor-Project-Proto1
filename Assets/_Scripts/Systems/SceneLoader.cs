fd27871e6a75a72cacd875cd1bf32aca18dbffea
Devansh Rawat <bikkarawat28@gmail.com>
2025-02-11 16:49:59 +0530

    LoadingScreenBasic/Cleanup

--- Assets/_Scripts/Systems/SceneLoader.cs
+++ Assets/_Scripts/Systems/SceneLoader.cs
@@ -2,17 +2,22 @@ using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace _Scripts.Systems
 {
     public class SceneLoader : Singleton<SceneLoader>
     {
         [SerializeField] private GameObject loadingCanvas;
-        [SerializeField] private float progress = 0;
+        [SerializeField] private Image progressBar;
+        [SerializeField] private float smoothTime;
+        private float _target;
+
         public void LoadScene<T>(T scene)
         {
-            string sceneName = scene is int 
-                ? SceneManager.GetSceneByBuildIndex(Convert.ToInt32(scene)).name 
+            string sceneName = scene is int
+                ? SceneManager.GetSceneByBuildIndex(Convert.ToInt32(scene)).name
                 : scene.ToString();
 
             StartCoroutine(LoadSceneAsync(sceneName));
@@ -20,32 +25,42 @@ namespace _Scripts.Systems
 
         private IEnumerator LoadSceneAsync(string sceneName)
         {
+            _target = 0;
+            progressBar.fillAmount = 0;
             loadingCanvas.SetActive(true);
-            
             var scene = SceneManager.LoadSceneAsync(sceneName);
-            
-            
-            scene.allowSceneActivation = false;
+            scene!.allowSceneActivation = false;
+            yield return new WaitForSeconds(0.2f);
+
+
 
             do
             {
-                progress = scene.progress;
+                _target = scene.progress;
                 if (scene.progress >= 0.9f)
                 {
-                    scene.allowSceneActivation = true;
+                    _target = 1;
 
-                    loadingCanvas.SetActive(false);
+                    scene.allowSceneActivation = true;
                 }
+
                 yield return null;
             } while (!scene.isDone);
 
+            yield return new WaitForSeconds(0.2f);
+            loadingCanvas.SetActive(false);
         }
-        
+
+        private void Update()
+        {
+            progressBar.fillAmount = Mathf.MoveTowards(progressBar.fillAmount, _target, smoothTime * Time.deltaTime);
+        }
+
     }
-}
 
 public enum SceneIndex
 {
     Menu,
     Main
+}
 }
